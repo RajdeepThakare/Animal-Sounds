0da4a8ec46f709fba150b7d61e7cf3a54edaf93e
Rajdeep100 <rajdeep.ravithakare@gmail.com>
2020-03-15 11:50:08 +0530

    padding improved 1.1

--- lib/pages/mammals.dart
+++ lib/pages/mammals.dart
@@ -27,158 +27,213 @@ class _MammalsState extends State<Mammals> {
         centerTitle: true,
       ),
       body: SingleChildScrollView(
-              child: Padding(
-          padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
-          child: Column(
-            children: <Widget>[
-              Row(
+              child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.fromLTRB(4,4,4,0),
+              child: Row(
                 children: <Widget>[
                   Builder(
                     builder: (context) => Container(
-                        height: 200,
-                        width: 200,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage('assets/images/mammals/cat.jpg'),
-                            
-                          ),
-                          borderRadius: new BorderRadius.all(new Radius.circular(50.0)),
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/mammals/cat.jpg'),
+                        ),
+                        borderRadius:
+                            new BorderRadius.all(new Radius.circular(50.0)),
+                        border: new Border.all(
+                          color: Colors.grey[800],
+                          width: 4.0,
+                        ),
+                      ),
+                      child: FlatButton(
+                        padding: EdgeInsets.all(0.0),
+                        onPressed: () {
+                          _audioCache.play('cat.mp3');
+                          //------------------------------
+                          Scaffold.of(context).showSnackBar(SnackBar(
+                            content: Text('Cat Meoww'),
+                            duration: Duration(milliseconds: 400),
+                          ));
+                          //-------------------------------
+                        },
+                        child: null,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 3),
+                  Builder(
+                    builder: (context) => Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/mammals/dog.jpg'),
+                        ),
+                        borderRadius:
+                            new BorderRadius.all(new Radius.circular(50.0)),
+                        border: new Border.all(
+                          color: Colors.grey[800],
+                          width: 4.0,
+                        ),
+                      ),
+                      child: FlatButton(
+                        padding: EdgeInsets.all(0.0),
+                        onPressed: () {
+                          _audioCache.play('dog.mp3');
+                          //------------------------------
+                          Scaffold.of(context).showSnackBar(SnackBar(
+                            content: Text('Dog Bark'),
+                            duration: Duration(milliseconds: 400),
+                          ));
+                          //-------------------------------
+                        },
+                        child: null,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(4,4,4,0),
+              child: Row(
+                children: <Widget>[
+                  Builder(
+                    builder: (context) => Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/mammals/pig.jpg'),
+                        ),
+                        borderRadius:
+                            new BorderRadius.all(new Radius.circular(50.0)),
+                        border: new Border.all(
+                          color: Colors.grey[800],
+                          width: 4.0,
+                        ),
+                      ),
+                      child: FlatButton(
+                        padding: EdgeInsets.all(0.0),
+                        onPressed: () {
+                          _audioCache.play('pig.mp3');
+                          //------------------------------
+                          Scaffold.of(context).showSnackBar(SnackBar(
+                            content: Text('Pig Squeal'),
+                            duration: Duration(milliseconds: 400),
+                          ));
+                          //-------------------------------
+                        },
+                        child: null,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 3),
+                  Builder(
+                    builder: (context) => Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/mammals/rhino.jpg'),
+                        ),
+                        borderRadius:
+                            new BorderRadius.all(new Radius.circular(50.0)),
+                        border: new Border.all(
+                          color: Colors.grey[800],
+                          width: 4.0,
+                        ),
+                      ),
+                      child: FlatButton(
+                        padding: EdgeInsets.all(0.0),
+                        onPressed: () {
+                          _audioCache.play('rhino.mp3');
+                          //------------------------------
+                          Scaffold.of(context).showSnackBar(SnackBar(
+                            content: Text('Rhinos .....'),
+                            duration: Duration(milliseconds: 400),
+                          ));
+                          //-------------------------------
+                        },
+                        child: null,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(4,4,4,4),
+              child: Row(
+                children: <Widget>[
+                  Builder(
+                    builder: (context) => Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/mammals/lamb.jpg'),
+                        ),
+                        borderRadius:
+                              new BorderRadius.all(new Radius.circular(50.0)),
                           border: new Border.all(
-                   color: Colors.red,
-                   width: 4.0,)
+                            color: Colors.grey[800],
+                            width: 4.0,
+                          ),
+                      ),
+                      child: FlatButton(
+                        padding: EdgeInsets.all(0.0),
+                        onPressed: () {
+                          _audioCache.play('lamb.wav');
+                          //------------------------------
+                          Scaffold.of(context).showSnackBar(SnackBar(
+                            content: Text('Lamb Baaa'),
+                            duration: Duration(milliseconds: 400),
+                          ));
+                          //-------------------------------
+                        },
+                        child: null,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 3),
+                  Builder(
+                    builder: (context) => Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/mammals/horse.jpg'),
                         ),
-                        child: FlatButton(
-                          padding: EdgeInsets.all(0.0),
-                          onPressed: () {
-                            _audioCache.play('cat.mp3');
-                            //------------------------------
-                            Scaffold.of(context).showSnackBar(SnackBar(
-                              content: Text('Cat Meoww'),
-                              duration: Duration(milliseconds: 400),
-                            ));
-                            //-------------------------------
-                          },
-                          child: null,
+                        borderRadius:
+                            new BorderRadius.all(new Radius.circular(50.0)),
+                        border: new Border.all(
+                          color: Colors.grey[800],
+                          width: 4.0,
                         ),
                       ),
-                  ),
-                  Builder(
-                    builder: (context) => ClipOval(
-                      child: Container(
-                        height: 200,
-                        width: 200,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage('assets/images/mammals/dog.jpg'),
-                          ),
-                        ),
-                        child: FlatButton(
-                          padding: EdgeInsets.all(0.0),
-                          onPressed: () {
-                            _audioCache.play('dog.mp3');
-                            //------------------------------
-                            Scaffold.of(context).showSnackBar(SnackBar(
-                              content: Text('Dog Bark'),
-                              duration: Duration(milliseconds: 400),
-                            ));
-                            //-------------------------------
-                          },
-                          child: null,
-                        ),
+                      child: FlatButton(
+                        padding: EdgeInsets.all(0.0),
+                        onPressed: () {
+                          _audioCache.play('horse.wav');
+                          //------------------------------
+                          Scaffold.of(context).showSnackBar(SnackBar(
+                            content: Text('Horse Whynning'),
+                            duration: Duration(milliseconds: 400),
+                          ));
+                          //-------------------------------
+                        },
+                        child: null,
                       ),
                     ),
                   ),
                 ],
               ),
-              Row(
-                children: <Widget>[
-                  
-                  Builder(
-                    builder: (context) => ClipOval(
-                      child: Container(
-                        height: 200,
-                        width: 200,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage('assets/images/mammals/pig.jpg'),
-                          ),
-                        ),
-                        child: FlatButton(
-                          padding: EdgeInsets.all(0.0),
-                          onPressed: () {
-                            _audioCache.play('pig.mp3');
-                            //------------------------------
-                            Scaffold.of(context).showSnackBar(SnackBar(
-                              content: Text('Pig Squeal'),
-                              duration: Duration(milliseconds: 400),
-                            ));
-                            //-------------------------------
-                          },
-                          child: null,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  Builder(
-                    builder: (context) => ClipOval(
-                      child: Container(
-                        height: 200,
-                        width: 200,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage('assets/images/mammals/lamb.jpg'),
-                          ),
-                        ),
-                        child: FlatButton(
-                          padding: EdgeInsets.all(0.0),
-                          onPressed: () {
-                            _audioCache.play('lamb.wav');
-                            //------------------------------
-                            Scaffold.of(context).showSnackBar(SnackBar(
-                              content: Text('Lamb Baaa'),
-                              duration: Duration(milliseconds: 400),
-                            ));
-                            //-------------------------------
-                          },
-                          child: null,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Builder(
-                    builder: (context) => ClipOval(
-                      child: Container(
-                        height: 200,
-                        width: 200,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage('assets/images/mammals/horse.jpg'),
-                          ),
-                        ),
-                        child: FlatButton(
-                          padding: EdgeInsets.all(0.0),
-                          onPressed: () {
-                            _audioCache.play('horse.wav');
-                            //------------------------------
-                            Scaffold.of(context).showSnackBar(SnackBar(
-                              content: Text('Horse Whynning'),
-                              duration: Duration(milliseconds: 400),
-                            ));
-                            //-------------------------------
-                          },
-                          child: null,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
