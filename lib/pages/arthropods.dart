b99f789057d978b8a4804d42fb06343708cacee9
Rajdeep100 <rajdeep.ravithakare@gmail.com>
2020-03-16 16:40:03 +0530

    Arthropods added

--- lib/pages/arthropods.dart
+++ lib/pages/arthropods.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class Arthropods extends StatefulWidget {
   @override
@@ -6,6 +8,18 @@ class Arthropods extends StatefulWidget {
 }
 
 class _ArthropodsState extends State<Arthropods> {
+  
+
+  AudioCache _audioCache;
+
+  @override
+  void initState() {
+    super.initState();
+    // create this only once
+    _audioCache = AudioCache(
+        prefix: "audio/arthropods/",
+        fixedPlayer: AudioPlayer()..setReleaseMode(ReleaseMode.STOP));
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,7 +27,216 @@ class _ArthropodsState extends State<Arthropods> {
         title: Text("Arthropods Here!!"),
         centerTitle: true,
       ),
-      body: Text("Arthoiy data"),
+      body: SingleChildScrollView(
+              child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.fromLTRB(4,4,4,0),
+              child: Row(
+                children: <Widget>[
+                  Builder(
+                    builder: (context) => Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/arthropods/bee.jpg'),
+                        ),
+                        borderRadius:
+                            new BorderRadius.all(new Radius.circular(50.0)),
+                        border: new Border.all(
+                          color: Colors.grey[800],
+                          width: 4.0,
+                        ),
+                      ),
+                      child: FlatButton(
+                        padding: EdgeInsets.all(0.0),
+                        onPressed: () {
+                          _audioCache.play('bee.m4a');
+                          //------------------------------
+                          Scaffold.of(context).showSnackBar(SnackBar(
+                            content: Text('Bee...'),
+                            duration: Duration(milliseconds: 400),
+                          ));
+                          //-------------------------------
+                        },
+                        child: null,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 3),
+                  Builder(
+                    builder: (context) => Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/arthropods/bumblebee.jpg'),
+                        ),
+                        borderRadius:
+                            new BorderRadius.all(new Radius.circular(50.0)),
+                        border: new Border.all(
+                          color: Colors.grey[800],
+                          width: 4.0,
+                        ),
+                      ),
+                      child: FlatButton(
+                        padding: EdgeInsets.all(0.0),
+                        onPressed: () {
+                          _audioCache.play('bumblebee.m4a');
+                          //------------------------------
+                          Scaffold.of(context).showSnackBar(SnackBar(
+                            content: Text('Bumble Bee....'),
+                            duration: Duration(milliseconds: 400),
+                          ));
+                          //-------------------------------
+                        },
+                        child: null,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(4,4,4,0),
+              child: Row(
+                children: <Widget>[
+                  Builder(
+                    builder: (context) => Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/arthropods/cicada.jpg'),
+                        ),
+                        borderRadius:
+                            new BorderRadius.all(new Radius.circular(50.0)),
+                        border: new Border.all(
+                          color: Colors.grey[800],
+                          width: 4.0,
+                        ),
+                      ),
+                      child: FlatButton(
+                        padding: EdgeInsets.all(0.0),
+                        onPressed: () {
+                          _audioCache.play('cicada.m4a');
+                          //------------------------------
+                          Scaffold.of(context).showSnackBar(SnackBar(
+                            content: Text('Cicada...'),
+                            duration: Duration(milliseconds: 400),
+                          ));
+                          //-------------------------------
+                        },
+                        child: null,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 3),
+                  Builder(
+                    builder: (context) => Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/arthropods/cockroach.jpg'),
+                        ),
+                        borderRadius:
+                            new BorderRadius.all(new Radius.circular(50.0)),
+                        border: new Border.all(
+                          color: Colors.grey[800],
+                          width: 4.0,
+                        ),
+                      ),
+                      child: FlatButton(
+                        padding: EdgeInsets.all(0.0),
+                        onPressed: () {
+                          _audioCache.play('cockroach.m4a');
+                          //------------------------------
+                          Scaffold.of(context).showSnackBar(SnackBar(
+                            content: Text('cockroach .....'),
+                            duration: Duration(milliseconds: 400),
+                          ));
+                          //-------------------------------
+                        },
+                        child: null,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(4,4,4,4),
+              child: Row(
+                children: <Widget>[
+                  Builder(
+                    builder: (context) => Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/arthropods/crab.jpg'),
+                        ),
+                        borderRadius:
+                              new BorderRadius.all(new Radius.circular(50.0)),
+                          border: new Border.all(
+                            color: Colors.grey[800],
+                            width: 4.0,
+                          ),
+                      ),
+                      child: FlatButton(
+                        padding: EdgeInsets.all(0.0),
+                        onPressed: () {
+                          _audioCache.play('crab.m4a');
+                          //------------------------------
+                          Scaffold.of(context).showSnackBar(SnackBar(
+                            content: Text('Crab...'),
+                            duration: Duration(milliseconds: 400),
+                          ));
+                          //-------------------------------
+                        },
+                        child: null,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 3),
+                  Builder(
+                    builder: (context) => Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/arthropods/mosquito.jpg'),
+                        ),
+                        borderRadius:
+                            new BorderRadius.all(new Radius.circular(50.0)),
+                        border: new Border.all(
+                          color: Colors.grey[800],
+                          width: 4.0,
+                        ),
+                      ),
+                      child: FlatButton(
+                        padding: EdgeInsets.all(0.0),
+                        onPressed: () {
+                          _audioCache.play('mosquito.m4a');
+                          //------------------------------
+                          Scaffold.of(context).showSnackBar(SnackBar(
+                            content: Text('Mosquito...'),
+                            duration: Duration(milliseconds: 400),
+                          ));
+                          //-------------------------------
+                        },
+                        child: null,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
